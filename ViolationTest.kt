772ebd825d06a4ff52885f1012498082a27b934f
MinJunKweon <minz.kweon@gmail.com>
2022-02-22 01:03:03 +0900

    Add kotlint code to test

---
+++ ViolationTest.kt
@@ -0,0 +1,17 @@
+import java.math.BigDecimal
+import kotlinx.coroutines.*
+import java.util.* // ktlint-disable no-wildcard-imports
+
+abstract class Parent()
+
+/* ktlint-disable colon-spacing */
+data class Data(
+    val foo : Int,
+    val bar: Int
+): Parent()
+/* ktlint-enable colon-spacing */
+
+fun main() {
+    val number : BigDecimal = BigDecimal(10)
+    println(number)
+}
