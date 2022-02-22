9991a1f018c3a2a3366764e13132c6088a7dd28e
MinJunKweon <minz.kweon@gmail.com>
2022-02-23 00:01:33 +0900

    Run lint and create review about results

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Step 0. Setup environment
-export KTLINT_VERSION=$1
-export GITHUB_TOKEN=$2
-
 # Step 1. Install ktlint
-if [ "$KTLINT_VERSION" = "latest" ] ; then
+if [ "$INPUT_KTLINT_VERSION" = "latest" ] ; then
   curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest \
     | grep "browser_download_url.*ktlint\"" \
     | cut -d : -f 2,3 \
@@ -14,13 +10,10 @@ if [ "$KTLINT_VERSION" = "latest" ] ; then
     && chmod a+x ktlint \
     && mv ktlint /usr/local/bin/
 else
-  curl -sSLO https://github.com/pinterest/ktlint/releases/download/"$KTLINT_VERSION"/ktlint \
+  curl -sSLO https://github.com/pinterest/ktlint/releases/download/"$INPUT_KTLINT_VERSION"/ktlint \
     && chmod a+x ktlint \
     && mv ktlint /usr/local/bin/
 fi
 
-# Step 2. Run ktlint
-ktlint --reporter=json,output=report.json --verbose || true
-
-# Step 3. run nodejs
+# Step 2. run nodejs
 node index.js
--- index.js
+++ index.js
@@ -1,11 +1,129 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+const { exec } = require('child_process');
 
-async function main() {
-    const token = core.getInput('github_token') || process.env.GITHUB_TOKEN;
-    const octokit = github.getOctokit(token);
+const REPORT_FILENAME = 'report.json'
 
-    console.log(octokit);
+const fetchGithubEvent = () => {
+    const githubEventPath = fs.readFileSync(process.env.GITHUB_EVENT_PATH);
+    return JSON.parse(githubEventPath);
 }
 
-main()
+const context = (() => {
+    const event = fetchGithubEvent();
+
+    const owner = process.env.GITHUB_REPOSITORY_OWNER;
+    const repo = event.repository.name;
+    const pullRequestNo = event.number;
+
+    return { owner, repo, pullRequestNo }
+})();
+
+const fetchChangedKotlinFiles = async (octokit) => {
+    const files = await octokit.paginate(octokit.rest.pulls.listFiles, {
+        owner: context.owner,
+        repo: context.repo,
+        pull_number: context.pullRequestNo,
+    });
+    console.log('::group::files');
+    console.log(files);
+    console.log('::endgroup::');
+
+    const filenames = files.map((file) => file.filename)
+
+    return filenames
+}
+
+const runKtlint = async (filenames) => {
+    const workspace = process.env.GITHUB_WORKSPACE;
+    const filepaths = filenames.map((filename) => workspace.concat('/', filename));
+    const filepathArgument = ''.concat('"', filepaths.join('" "'), '"');
+
+    return new Promise((resolve, reject) => {
+        // run ktlint
+        const command = `ktlint --reporter=json,output=${REPORT_FILENAME} --relative --verbose ${filepathArgument}`
+        console.log(command);
+        exec(command, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ stdout, stderr });
+            }
+        });
+    });
+}
+
+const readLintResults = () => {
+    const reportJson = fs.readFileSync(REPORT_FILENAME);
+    return JSON.parse(reportJson);
+}
+
+const printResultOfFile = (resultFile) => {
+    const filename = resultFile.file;
+    resultFile.errors.forEach(error => {
+        const location = `${error.line}:${error.column}`;
+        const message = error.message;
+        console.log(`${filename}:${location} :: ${message} (${error.rule})`);
+    });
+}
+
+const printResult = (result) => {
+    console.log('::group::result');
+    result.forEach(printResultOfFile);
+    console.log('::endgroup::');
+}
+
+const createReviewComment = (filename, violation) => {
+    const comment = `:warning: **${violation.rule}**\n\n${violation.message}`
+    return {
+        path: filename,
+        body: comment,
+        line: violation.line,
+    }
+}
+
+const postGithubReview = async (octokit, results) => {
+    const reviewComments = results.flatMap(result => {
+        const filename = result.file;
+        return result.errors.map(violation => createReviewComment(filename, violation))
+    })
+
+    await octokit.rest.pulls.createReview({
+        owner: context.owner,
+        repo: context.repo,
+        pull_number: context.pullRequestNo,
+        body: `:hammer: **[ktlint]** There are ${reviewComments.length} Violations`,
+        event: 'REQUEST_CHANGES',
+        comments: reviewComments,
+    });
+}
+
+const main = async () => {
+    const token = core.getInput('github_token') || process.env.INPUT_GITHUB_TOKEN;
+    const octokit = github.getOctokit(token);
+
+
+    const filenames = await fetchChangedKotlinFiles(octokit);
+
+    console.log("::group::Lint Files");
+    filenames.map((filename) => console.log(filename));
+    console.log("::endgroup::");
+
+    try {
+        await runKtlint(filenames);
+        return;
+    } catch (err) {
+        const results = readLintResults();
+        printResult(results);
+        try {
+            await postGithubReview(octokit, results);
+            console.log('Succeed to post Github PR Review');
+        } catch (err) {
+            console.log('Failed to post Github PR Review.');
+            throw err;
+        }
+    }
+}
+
+main();
